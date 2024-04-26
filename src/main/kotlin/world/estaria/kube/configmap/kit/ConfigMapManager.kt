68a184d5330539cd0b172ce668867e25e84ea551
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-04-26 11:33:37 +0200

    implement all methods in ConfigMapManager

--- src/main/kotlin/world/estaria/kube/configmap/kit/ConfigMapManager.kt
+++ src/main/kotlin/world/estaria/kube/configmap/kit/ConfigMapManager.kt
@@ -2,7 +2,9 @@ package world.estaria.kube.configmap.kit
 
 import com.charleskorn.kaml.Yaml
 import io.fabric8.kubernetes.api.model.ConfigMap
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder
 import io.fabric8.kubernetes.client.KubernetesClient
+import io.fabric8.kubernetes.client.dsl.Resource
 import kotlinx.serialization.KSerializer
 
 /**
@@ -10,29 +12,66 @@ import kotlinx.serialization.KSerializer
  */
 
 class ConfigMapManager(
+    private val namespace: String,
     private val kubernetesClient: KubernetesClient
 ) {
 
     private val cachedConfigMaps = hashMapOf<String, ConfigMap>()
 
-    fun <C> migrateConfigToImage(name: String, serializer: KSerializer<C>) {
-
+    /**
+     * Creates a new config map
+     * @param name of the config
+     * @param serializer of the config class
+     * @param value what you want to save in the config
+     */
+    fun <C> migrateConfigToImage(name: String, serializer: KSerializer<C>, value: C) {
+        val yamlString = Yaml.default.encodeToString(serializer, value)
+        this.kubernetesClient.configMaps()
+            .inNamespace(this.namespace)
+            .resource(createConfigMap(name, yamlString))
+            .create()
     }
 
+    /**
+     * Updates the cached config
+     * @param name of the config
+     */
     fun updateConfig(name: String) {
-        this.cachedConfigMaps[name] = getConfigMapFromKubernetes(name)
+        this.cachedConfigMaps[name] = getConfigMapFromKubernetes(name).get()
     }
 
+    /**
+     * Returns true if this config is ready
+     * @param name of the config
+     * @return true or false :)
+     */
+    fun existsConfig(name: String): Boolean {
+        return getConfigMapFromKubernetes(name).isReady
+    }
+
+    /**
+     * Gets the config class of the [ConfigMap]
+     * @param name of the config
+     * @param serializer of the config class
+     * @return config class instance
+     */
     fun <C> getConfig(name: String, serializer: KSerializer<C>): C {
-        val configMap = this.cachedConfigMaps[name] ?: getConfigMapFromKubernetes(name)
-        return Yaml.default.decodeFromString(serializer, "")
+        val configMap = this.cachedConfigMaps[name] ?: getConfigMapFromKubernetes(name).get()
+        val string = configMap.data[name] ?: throw NullPointerException("failed to find $name config")
+        return Yaml.default.decodeFromString(serializer, string)
     }
 
-    private fun getConfigMapFromKubernetes(name: String): ConfigMap {
+    private fun getConfigMapFromKubernetes(name: String): Resource<ConfigMap> {
         return this.kubernetesClient.configMaps()
-            .inNamespace("default")
+            .inNamespace(this.namespace)
             .withName(name)
-            .get()
+    }
+
+    private fun createConfigMap(name: String, yamlString: String): ConfigMap {
+        val configMap = ConfigMap()
+        configMap.metadata = ObjectMetaBuilder().withName(name).build()
+        configMap.data = hashMapOf(Pair(name, yamlString))
+        return configMap
     }
 
 }
