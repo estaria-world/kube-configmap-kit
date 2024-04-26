1c789e691fffefd9865f96e25608c674e6d78a6e
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-04-26 20:59:47 +0200

    add new method in KubeConfigMapKit

--- src/main/kotlin/world/estaria/kube/configmap/kit/KubeConfigMapKit.kt
+++ src/main/kotlin/world/estaria/kube/configmap/kit/KubeConfigMapKit.kt
@@ -1,5 +1,6 @@
 package world.estaria.kube.configmap.kit
 
+import io.fabric8.kubernetes.client.KubernetesClient
 import io.fabric8.kubernetes.client.KubernetesClientBuilder
 
 /**
@@ -15,6 +16,16 @@ object KubeConfigMapKit {
      */
     fun initializeKubeConfig(namespace: String): ConfigMapManager {
         val kubernetesClient = KubernetesClientBuilder().build()
+        return ConfigMapManager(namespace, kubernetesClient)
+    }
+
+    /**
+     * Initialize a kubernetes client for the configMaps
+     * @param namespace where the config is stored
+     * @param kubernetesClient the kubernetes client
+     * @return new instance of [ConfigMapManager]
+     */
+    fun initializeKubeConfig(namespace: String, kubernetesClient: KubernetesClient): ConfigMapManager {
         return ConfigMapManager(namespace, kubernetesClient)
     }
 
